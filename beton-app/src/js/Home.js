af79442c495fd459389f0e0047029078154c30a1
wayne <nigmatova923@gmail.com>
2023-09-07 19:34:29 +0500

    aefaef
    faefwe

--- beton-app/src/js/Home.js
+++ beton-app/src/js/Home.js
@@ -3,6 +3,12 @@ import '../css/Home.css'
 import iphone from '../img/iphone.jpeg'
 import pizza1 from '../img/pizza1.jpeg'
 import pizza2 from '../img/pizza2.jpeg'
+import ag from '../img/appgallery.jpeg'
+import ap from '../img/appstore.jpeg'
+import ge from '../img/getapps.jpeg'
+import go from '../img/googleplay.jpeg'
+import ru from '../img/rustore.jpeg'
+import {BsArrowRight} from 'react-icons/bs'
 
 export default function Home() {
   return (
@@ -18,11 +24,19 @@ export default function Home() {
                 <h1><span>Купить бетон</span> также просто, <br />как заказать пиццу</h1>
                 <p>Скачивай наше приложение!</p>
             </div>
+            <div className="progg">
+                <img src={ag} alt="" />
+                <img src={ap} alt="" />
+                <img src={ge} alt="" />
+                <img src={go} alt="" />
+                <img src={ru} alt="" />
+                <img src={ag} alt="" />
+            </div>
             <div className="b-right">
                 <img className='piz2' src={pizza2} alt="" />
                 <img className='piz1' src={pizza1} alt="" />
             </div>
-            <div className="dale"></div>
+            <div className="dale"><BsArrowRight className='icc'/></div>
         </div>
       </div>
     </div>
